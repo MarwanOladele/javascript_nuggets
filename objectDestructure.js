c8c65f5d3f398b38312b9ffbf258cdcb6b290a01
MarwanOladele <oladelemarwan@gmail.com>
2023-05-31 14:29:10 +0100

    object destructure

---
+++ objectDestructure.js
@@ -0,0 +1,33 @@
+const bob = {
+  firstName: "Marwan",
+  lastName: "Oladele",
+  city: "Nigeria",
+  siblings: {
+    name: "Hikmah",
+  },
+};
+
+// const firstName = bob.firstName;
+// const lastName = bob.lastName;
+// const sister = bob.siblings.name;
+
+// console.log(firstName, lastName, sister);
+
+const {
+  firstName: myName,
+  lastName,
+  city,
+  siblings: { name: onlySister },
+} = bob;
+console.log(myName, lastName, city, onlySister);
+
+const printName = ({
+  firstName: myName,
+  lastName,
+  city,
+  siblings: { name: onlySister },
+}) => {
+  console.log(myName, lastName, city, onlySister);
+};
+
+printName(bob);
